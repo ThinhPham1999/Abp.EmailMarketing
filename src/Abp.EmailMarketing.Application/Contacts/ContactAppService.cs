11cbfeabb400df2e6e80e6fe1abd9ff06be716ca
PC <thinhphu1234567@gmail.com>
2021-05-04 23:48:57 +0700

    Finish create server side

---
+++ src/Abp.EmailMarketing.Application/Contacts/ContactAppService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Abp.EmailMarketing.Contacts
+{
+    public class ContactAppService :
+        CrudAppService<
+            Contact,
+            ContactDto,
+            Guid,
+            PagedAndSortedResultRequestDto,
+            CreateUpdateContactDto,
+            IContactAppService>
+    {
+        public ContactAppService(IRepository<Contact, Guid> repository) : base(repository)
+        {
+
+        }
+    }
+}

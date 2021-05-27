4556541261a62bf85590ce8da98b345e58ad7e68
PC <PC@DESKTOP-9829D4F>
2021-05-27 08:39:35 +0700

    fix service email service

--- src/Abp.EmailMarketing.Domain/Emailing/AnotherEmailService.cs
+++ src/Abp.EmailMarketing.Domain/Emailing/AnotherEmailService.cs
@@ -1,5 +1,7 @@
 ﻿using MailKit.Net.Smtp;
 using MailKit.Security;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using MimeKit;
 using MimeKit.Text;
 using NETCore.MailKit.Core;
@@ -8,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
 
 namespace Abp.EmailMarketing.Emailing
 {
@@ -16,12 +19,13 @@ namespace Abp.EmailMarketing.Emailing
         void Send(string from, string to, string subject, string html);
     }*/
 
-    public class AnotherEmailService// : IAnotherEmailService
+    public class AnotherEmailService : ITransientDependency
     {
+        public ILogger<AnotherEmailService> Logger { get; set; }
 
         public AnotherEmailService()
         {
-
+            Logger = NullLogger<AnotherEmailService>.Instance;
         }
 
         public void Send(string from, string to, string subject, string html)
@@ -41,7 +45,7 @@ namespace Abp.EmailMarketing.Emailing
             smtp.Disconnect(true);
         }
 
-        public void Send(string from, string to, string subject, string html, string username, string password)
+        public async Task Send(string from, string to, string subject, string html, string username, string password)
         {
             // create message
             var email = new MimeMessage();
@@ -52,10 +56,43 @@ namespace Abp.EmailMarketing.Emailing
 
             // send email
             using var smtp = new SmtpClient();
-            smtp.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
-            smtp.Authenticate(username, password);
-            smtp.Send(email);
+            await smtp.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(username, password);
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
+
+        public async Task Send(EmailSetting emailSetting)
+        {
+            // create message
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(emailSetting.DisplayName));
+            email.To.Add(MailboxAddress.Parse(emailSetting.To));
+            email.Subject = emailSetting.Subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = emailSetting.Body };
+
+            // send email
+            using var smtp = new SmtpClient();
+            smtp.Connect(emailSetting.Host, emailSetting.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(emailSetting.Mail, emailSetting.Password);
+            await smtp.SendAsync(email);
             smtp.Disconnect(true);
+            Logger.LogInformation("send mail to " + emailSetting.To);
+        }
+
+        public async Task SendEmailAsync(string displayName,string from, string to, string subject, string html, string username, string password)
+        {
+            await Send(new EmailSetting()
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                Mail = username,
+                Password = password,
+                To = to,
+                Subject = subject,
+                Body = html,
+                DisplayName = displayName
+            });
         }
     }
 }
---
+++ src/Abp.EmailMarketing.Domain/Emailing/EmailSetting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abp.EmailMarketing.Emailing
+{
+    public class EmailSetting
+    {
+        public string Mail { get; set; }
+        public string DisplayName { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string To { get; set; }              // Địa chỉ gửi đến
+        public string Subject { get; set; }         // Chủ đề (tiêu đề email)
+        public string Body { get; set; }            // Nội dung (hỗ trợ HTML) của email
+    }
+}
